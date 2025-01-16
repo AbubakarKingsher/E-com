543e08febf27215e7327eca4b376307a2a5e7e01
AbubakarKingsher <abubakarmarri007@gmail.com>
2025-01-16 19:35:24 +0500

    makeing new

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Context from "./utils/Context.jsx";
 import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <Context>
     <BrowserRouter>
-        <App />
+      <App />
       <ToastContainer />
     </BrowserRouter>
   </Context>
