dccced7d5e42cab3ebf7dd1dd8ac8bcbdd7ec2ff
AbubakarKingsher <abubakarmarri007@gmail.com>
2025-01-16 21:10:08 +0500

    Updated the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Details from "./components/Details";
 import CreateProduct from "./components/CreateProduct";
@@ -8,13 +8,14 @@ import Edit from "./components/Edit";
 function App() {
   return (
     <div className="h-screen w-full flex">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path={`/details/:id`} element={<Details />} />
-        <Route path="/create" element={<CreateProduct />} />
-        <Route path="/edit/:id" element={<Edit />} />
-      </Routes>
-
+      <BrowserRouter basename="/E-com">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path={`/details/:id`} element={<Details />} />
+          <Route path="/create" element={<CreateProduct />} />
+          <Route path="/edit/:id" element={<Edit />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
